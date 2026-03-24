80d2371fb05b6868978ac76cc28063bfa101d644
solbin23 <solbin23@naver.com>
2026-03-24 13:49:33 +0900

    유저권한체크로직추가

--- src/main/java/com/cafe/cafeMood/common/auth/LoginUser.java
+++ src/main/java/com/cafe/cafeMood/common/auth/LoginUser.java
@@ -5,4 +5,14 @@ import com.cafe.cafeMood.user.domain.UserRole;
 public record LoginUser(Long userId,
                         String email,
                         UserRole role) {
+
+    public boolean isAdmin() {
+        return role == UserRole.ADMIN;
+    }
+    public boolean isUser() {
+        return role == UserRole.USER;
+    }
+    public boolean isOwner() {
+        return role == UserRole.OWNER;
+    }
 }
