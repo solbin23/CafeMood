d51c7cfcacbfbaf94077ca171bf44c90e52c4056
solbin23 <solbin23@naver.com>
2026-03-25 10:38:02 +0900

    securityConfig jwt빈 추가 및 필터체인 수정

--- src/main/java/com/cafe/cafeMood/common/config/SecurityConfig.java
+++ src/main/java/com/cafe/cafeMood/common/config/SecurityConfig.java
@@ -1,11 +1,14 @@
 package com.cafe.cafeMood.common.config;
 
+import com.cafe.cafeMood.common.auth.jwt.JwtAuthFilter;
+import com.cafe.cafeMood.common.auth.jwt.JwtTokenProvider;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,39 +16,42 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
 
-    @Value("${admin.id}")
-    private String adminId;
+    @Bean
+    public JwtTokenProvider jwtTokenProvider() {
+        String secret = "cafeMood";
+        long accessTokenValiditySeconds = 1000L * 60 * 30;
 
-    @Value("${admin.password}")
-    private String adminPassword;
+        return new JwtTokenProvider(secret, accessTokenValiditySeconds);
+    }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
+    public JwtAuthFilter jwtAuthFilter(JwtTokenProvider jwtTokenProvider) {
+        return new JwtAuthFilter(jwtTokenProvider);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http,JwtAuthFilter jwtAuthFilter) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .logout(AbstractHttpConfigurer::disable)
                 .sessionManagement(securitySessionManagement -> securitySessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .httpBasic(Customizer.withDefaults())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.GET, "/api/cafes/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/tags/**").permitAll()
-                        .anyRequest().permitAll());
+                        .anyRequest().permitAll())
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .cors(Customizer.withDefaults());
 
         return http.build();
 
     }
 
-    @Bean
-    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
-        return new InMemoryUserDetailsManager(
-                User.withUsername(adminId)
-                        .password(encoder.encode(adminPassword))
-                        .roles("ADMIN")
-                        .build()
-        );
-    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
